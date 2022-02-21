425a7e78000b56c83e53243692d1fb0951c7a37c
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2022-02-22 02:59:08 +0900

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,5 +1,5 @@
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-204980327-3']);
+_gaq.push(['_setAccount', 'UA-142558831-2']);
 _gaq.push(['_trackPageview']);
 (function() {
     var ga = document.createElement('script');
